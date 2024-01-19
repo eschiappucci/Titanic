a10921808531793623676c950507c13549114886
eschiappucci <61677647+eschiappucci@users.noreply.github.com>
2024-01-19 11:09:11 +1100

    Startint the ML

--- titanic_ml.py
+++ titanic_ml.py
@@ -1,10 +1,28 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+def tf_regression(mat, output_shape = 1, rate=1e-5, loss_func='mse', metric='mean_absolute_percentage_error'):
+    model = keras.Sequential([
+        keras.layers.ZeroPadding1D(2, input_shape=mat[0].shape),
+        keras.layers.LocallyConnected1D(128, 4, data_format='channels_last'),
+        keras.layers.LocallyConnected1D(64, 5, data_format='channels_last'),
+        keras.layers.GlobalAveragePooling1D(),
+        keras.layers.Dense(512, activation='elu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(256, activation='elu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(output_shape, activation='exponential')
+        ])
+    opt = tf.keras.optimizers.Adam(learning_rate=rate)
+    model.compile(optimizer=opt, loss=loss_func, metrics=[metric])
+    return model
 
-df_train = pd.read_csv('train.csv')
-df_test = pd.read_csv('test.csv')
+def plot_metric(history, metric='loss'):
+    plt.plot(history.history[metric], label=metric)
+    plt.xlabel('Epoch')
+    plt.ylabel('Change of '+metric)
+    plt.legend()
+    plt.show()
 
-print(df_train.head())
-
-plt.scatter(df_train['Age'], df_train['Survived'])
+def recompile(loc, rate=1e-5, loss_func='mse', metric='mean_absolute_percentage_error'):
+    model = tf.keras.models.load_model(loc, compile=False)
+    opt = tf.keras.optimizers.Adam(learning_rate=rate)
+    model.compile(optimizer=opt, loss=loss_func, metrics=[metric])
+    return model
