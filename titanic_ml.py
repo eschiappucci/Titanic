636a156805399ee2bbf79cf6e5498380b2a50923
Ed <edschiappucci@gmail.com>
2024-01-23 17:34:41 +1100

    ML working

--- titanic_ml.py
+++ titanic_ml.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 def tf_regression(mat, output_shape = 1, rate=1e-5, loss_func='binary_crossentropy', metric='binary_accuracy'):
     model = keras.Sequential([
-        keras.layers.Dense(256, input_shape=mat[0].shape),
-        keras.layers.BatchNormalization(),
-        keras.layers.Dense(512, activation='relu'),
-        keras.layers.BatchNormalization(),
+        keras.layers.Dense(128, input_shape=(mat.shape[-1],)),
+        #keras.layers.BatchNormalization(),
         keras.layers.Dense(256, activation='relu'),
-        keras.layers.BatchNormalization(),
+        #keras.layers.BatchNormalization(),
+        keras.layers.Dense(128, activation='relu'),
+        #keras.layers.BatchNormalization(),
         keras.layers.Dense(output_shape, activation='sigmoid')
         ])
     opt = tf.keras.optimizers.Adam(learning_rate=rate)
@@ -20,6 +20,13 @@ def plot_metric(history, metric='loss'):
     plt.plot(history.history[metric], label=metric)
     plt.xlabel('Epoch')
     plt.ylabel('Change of '+metric)
+    plt.legend()
+    plt.show()
+
+def plot_tot_history(history, label='None'):
+    plt.plot(history, label=label)
+    plt.xlabel('Epoch')
+    plt.ylabel(f'Change of {label}')
     plt.legend()
     plt.show()
 
